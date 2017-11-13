bd36b87225d8da1b06acfdb2a22e6087985d10d3
Roman Iakobchuk <kaboom.ua@gmail.com>
2017-11-13 19:09:15 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Root from './src/Root'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+          <Root/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
