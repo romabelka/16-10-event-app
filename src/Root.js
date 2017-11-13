47a9a6f034aa89b84cb6109f35e48d6964b65fa0
Roman Iakobchuk <kaboom.ua@gmail.com>
2017-11-13 19:58:40 +0200

    add image

--- src/Root.js
+++ src/Root.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-//import {View} from 'react-native'
+import {View, Image, StyleSheet} from 'react-native'
 //import HelloWorld from './HelloWorld'
 //import Auth from './auth/SignIn'
 //import EventScreen from './events/EventScreen'
@@ -9,7 +9,25 @@ import {eventList} from './fixtures'
 export default class Root extends Component {
     render() {
         return (
-            <EventList events = {eventList} />
+            <View style = {styles.container}>
+                <Image
+                    source = {require('../assets/images/logo.png')}
+                    style = {styles.logo}
+                    resizeMode={Image.resizeMode.contain}
+                />
+                <EventList events = {eventList} />
+            </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    logo: {
+        width: '100%',
+        height: 100
+    },
+    container: {
+        marginTop: 100,
+        width: '100%'
+    }
+})
