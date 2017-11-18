496fc7fc1a1fd5a74d426259123e1fa86d468ad3
evpozdniakov <evpozdniakov@gmail.com>
2017-11-18 18:49:10 -0500

    Apply firebase config; improve SignIn look.

--- src/fbSetup.js
+++ src/fbSetup.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
-const config = {
-    apiKey: "AIzaSyB31xpTtp4Jln_hb2kAbE4PGf6Mi8EgLyA",
-    authDomain: "advreact-1610.firebaseapp.com",
-    databaseURL: "https://advreact-1610.firebaseio.com",
-    projectId: "advreact-1610",
-    storageBucket: "advreact-1610.appspot.com",
-    messagingSenderId: "397157634637"
-};
+export const appName = 'advreact-1610-9cb0c'
 
-firebase.initializeApp(config);
+firebase.initializeApp({
+    apiKey: "AIzaSyBNaOQkLL75ZGoeaNtqbe63wEjjWLzLOPY",
+    authDomain: `${appName}.firebaseapp.com`,
+    databaseURL: `https://${appName}.firebaseio.com`,
+    projectId: appName,
+    storageBucket: `${appName}.appspot.com`,
+    messagingSenderId: "266748171955"
+})
